e6f28b575221e5cffd9813684fdc2c39aa6d5723
karthikraja <pmnkarthikraja@gmail.com>
2024-04-07 13:23:48 +0530

    add package lint and fix lint

--- frontend/src/components/useUsersTracking.ts
+++ frontend/src/components/useUsersTracking.ts
@@ -4,15 +4,15 @@ import { useEmail } from "./useEmail"
 
 export const useUsersTracking = (currentUser: UserDetail, userDetails: UserDetail[]): ReferralTrack[] => {
     const emails = useEmail()
-    const refereeEmails = emails.emails.filter(mail => mail.referees.find(r => r.email == currentUser.email))
+    const refereeEmails = emails.emails.filter(mail => mail.referees.find(r => r.email === currentUser.email))
 
     const referralTracks: ReferralTrack[] = refereeEmails.map(refereeEmail => {
         if (!!userDetails || userDetails !== undefined) {
             let status: ReferralStatus = 'Pending'
-            const gotUser = userDetails.find(us => refereeEmail.address == us.email)
+            const gotUser = userDetails.find(us => refereeEmail.address === us.email)
             if (!!gotUser) {
                 status = 'Failed'
-                status = gotUser.opReferralCode == currentUser.ownReferralCode ? 'Success' : status
+                status = gotUser.opReferralCode === currentUser.ownReferralCode ? 'Success' : status
             }
             return {
                 email: refereeEmail.address,
